a70b2b32bb18b6ce18e84ed1343821f5bc3b4d4a
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2019-01-03 12:35:00 +0100

    pdflatex now takes string input, making the write() function no longer necessary.

--- kanbancard/latex.py
+++ kanbancard/latex.py
@@ -1,14 +1,17 @@
 """
 Latex templating code, templated with Jinja2 and rendered with pdflatex.
-
-modified from http://eosrei.net/articles/2015/11/latex-templates-python-and-jinja2-generate-pdfs
 """
 
 import os
 import jinja2
 import subprocess
 
-def get_latex_template(tex_filename):
+def fill_latex_template(tex_filename, **options):
+    """
+    Renders a latex file template with data from the options dict, lookcing for \VAR{} and \BLOCK{} in the template.
+
+    Approach taken from http://eosrei.net/articles/2015/11/latex-templates-python-and-jinja2-generate-pdfs
+    """
     env = jinja2.Environment(
         block_start_string='\BLOCK{',
         block_end_string='}',
@@ -22,15 +25,16 @@ def get_latex_template(tex_filename):
         autoescape=False,
         loader=jinja2.FileSystemLoader(os.path.abspath('.'))
     )
-    return env.get_template(tex_filename)
+    template = env.get_template(tex_filename)
+    rendered_tex = template.render(**options)
+    return rendered_tex
 
 
 def pdflatex(tex, output_dir='.'):
     subprocess.run(r'pdflatex -output-directory {dir}'.format(dir=output_dir), input=tex.encode())
 
 
-def render_to_pdf(tex_filename, ouput_filename, output_dir, options):
+def render_to_pdf(tex_filename, output_dir, options):
     """Do full rendering pipeline, passing options dict to the jinja2 latex template and building with pdflatex."""
-    template = get_latex_template(tex_filename)
-    renderer_template = template.render(**options)
-    pdflatex(tex=renderer_template, output_dir=output_dir)
+    tex = fill_latex_template(tex_filename, **options)
+    pdflatex(tex=tex, output_dir=output_dir)
