9de3879a4f8ca2103e1c8f83dc56f35df330285d
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2019-01-07 21:29:58 +0100

    moved pdf generation function to submodule

--- kanbancard/__init__.py
+++ kanbancard/__init__.py
@@ -1,3 +1,4 @@
 __version__ = '0.1.0'
 
 from . import latex
+from .card import generate_pdfs
--- run.py
+++ run.py
@@ -1,18 +1,9 @@
-import os
-from os import path
-from datetime import datetime
-import pandas as pd
-from kanbancard import latex
 from flask import Flask, render_template, request, make_response
-from io import BytesIO
-from uuid import uuid4
-from PyPDF2 import PdfFileMerger
-import xlrd
-
-
+import kanbancard
 
 app = Flask(__name__)
 
+template_file = 'templates/card_template.tex'
 csv_save_dir = 'out_files'
 
 
@@ -24,7 +15,7 @@ def index():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
 	filename, csv_data = request.files['csv'].filename, request.files['csv'].read()
-	pdf = generate_pdfs(data_filename=filename, csv_data=csv_data)
+	pdf = kanbancard.generate_pdfs(data_filename=filename, csv_data=csv_data, csv_save_dir=csv_save_dir, template_file=template_file)
 	response = make_response(pdf)
 	response.headers['Content-Disposition'] = "inline; filename='booking.pdf"
 	response.mimetype = 'application/pdf'
@@ -39,58 +30,6 @@ def download_example():
 	response.headers['Content-Disposition'] = "inline; filename='example_sequence.xlsx"
 	response.mimetype = 'application/vnd.ms-excel'
 	return response
-
-
-def generate_pdfs(data_filename, csv_data, template_file='templates/card_template.tex'):
-
-	# Read / Validate CSV Sequence File
-	if 'csv' in path.splitext(data_filename)[1]:
-		batch_name = str(uuid4()).split('-')[0]
-		df = pd.read_csv(BytesIO(csv_data), skiprows=[0])
-		batches = {batch_name: df}
-		pre_header = csv_data.splitlines()[0].decode()
-	else:
-		excel_reader = pd.ExcelFile(BytesIO(csv_data))
-		batches = {sheet_name: excel_reader.parse(sheet_name, skiprows=[0]) for sheet_name in excel_reader.sheet_names}
-		pre_header = xlrd.open_workbook(file_contents=csv_data).sheet_by_name('1').row(0)[0].value
-
-	pdf_merger = PdfFileMerger()
-	for batch_name, df in batches.items():
-
-		metadata = dict([el.strip().lstrip() for el in item.split(':')] for item in df['Comment'][0].split(','))
-
-		# Render to PDF Card via Latex
-		options = {
-			'Project': metadata['Project'],
-			'BatchID': batch_name,
-			'Date': datetime.now().strftime('%d.%m.%Y'),
-			'Filename': path.basename(data_filename),
-			'df': df,
-			'Comments': metadata,
-			'Researcher': metadata['Researcher'],
-		}
-
-
-		with open(template_file) as f:
-			tex = latex.render_templated_tex(tex=f.read(), **options)
-
-		if not path.exists(csv_save_dir):
-			os.mkdir(csv_save_dir)
-
-		new_filename = "{}_{}.csv".format(path.join(csv_save_dir, path.splitext(path.basename(data_filename))[0]), batch_name)
-		with open(new_filename, 'w') as f:
-			f.write(pre_header + '\n')
-			df.to_csv(f, index_label=False, index=False)
-
-
-		pdf = latex.pdflatex(tex=tex)
-		pdf_merger.append(BytesIO(pdf))
-
-	with BytesIO() as f:
-		pdf_merger.write(f)
-		f.seek(0)
-		return f.read()
-
 
 
 if __name__ == '__main__':
