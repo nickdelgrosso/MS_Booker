b33270be112bf2f8ad98bf73f9647249ee5fa198
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2019-01-09 23:14:16 +0100

    stripped out file-saving, moving csv creation to a currently-unused function.

--- kanbancard/card.py
+++ kanbancard/card.py
@@ -1,18 +1,18 @@
-import os
 from os import path
 from datetime import datetime
-import pandas as pd
-from . import latex
-from io import BytesIO
 from uuid import uuid4
+from . import latex
 
 
-def generate_pdfs(data_filename, csv_data, csv_save_dir, template_file):
+def df_to_xcalibur_csv(df, bracket=4):
+    """Returns an XCalibur-formatted csv as a string (with 'bracket' put in the header), given a pandas DataFrame."""
+    return 'Bracket Type={}{}\n{}'.format(bracket, ',' * (df.shape[1] - 1), df.to_csv(index=False))
+
+
+def generate_pdfs(df, filename, template_file):
 
     # Read / Validate CSV Sequence File
     batch_name = str(uuid4()).split('-')[0]
-    df = pd.read_csv(BytesIO(csv_data), skiprows=[0])
-    pre_header = csv_data.splitlines()[0].decode()
 
     metadata = dict([el.strip().lstrip() for el in item.split(':')] for item in df['Comment'][0].split(','))
 
@@ -21,24 +21,14 @@ def generate_pdfs(data_filename, csv_data, csv_save_dir, template_file):
         'Project': metadata['Project'],
         'BatchID': batch_name,
         'Date': datetime.now().strftime('%d.%m.%Y'),
-        'Filename': path.basename(data_filename),
+        'Filename': path.basename(filename),
         'df': df,
         'Comments': metadata,
         'Researcher': metadata['Researcher'],
     }
 
-
     with open(template_file) as f:
         tex = latex.render_templated_tex(tex=f.read(), **options)
-
-    if not path.exists(csv_save_dir):
-        os.mkdir(csv_save_dir)
-
-    new_filename = "{}_{}.csv".format(path.join(csv_save_dir, path.splitext(path.basename(data_filename))[0]), batch_name)
-    with open(new_filename, 'w') as f:
-        f.write(pre_header + '\n')
-        df.to_csv(f, index_label=False, index=False)
-
 
     pdf = latex.pdflatex(tex=tex)
     return pdf
--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, make_response
+import pandas as pd
+from io import BytesIO
 import kanbancard
+
 
 app = Flask(__name__)
 
 template_file = 'templates/card_template.tex'
-csv_save_dir = 'out_files'
 
 
 @app.route('/')
@@ -15,7 +17,8 @@ def index():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
 	filename, csv_data = request.files['csv'].filename, request.files['csv'].read()
-	pdf = kanbancard.generate_pdfs(data_filename=filename, csv_data=csv_data, csv_save_dir=csv_save_dir, template_file=template_file)
+	df = pd.read_csv(BytesIO(csv_data), skiprows=[0])
+	pdf = kanbancard.generate_pdfs(df, filename=filename, template_file=template_file)
 	response = make_response(pdf)
 	response.headers['Content-Disposition'] = "inline; filename='booking.pdf"
 	response.mimetype = 'application/pdf'
