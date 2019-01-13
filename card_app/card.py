5aeadb9bb2a5013c86039472c16dd35378a0b8e6
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2019-01-13 20:33:49 +0100

    added methods data to pdf

--- card_app/card.py
+++ card_app/card.py
@@ -6,10 +6,11 @@ def df_to_xcalibur_csv(df, bracket=4):
     return 'Bracket Type={}{}\n{}'.format(bracket, ',' * (df.shape[1] - 1), df.to_csv(index=False))
 
 
-def df_to_card_pdf(template_tex, df, filename, batch_id, date):
+def df_to_card_pdf(template_tex, df, filename, batch_id, date, lc_settings):
     comments = dict([el.strip() for el in item.split(':')] for item in df['Comment'][0].split(','))
     samples = [row for _, row in df.iterrows()]
+    lc_settings = lc_settings.to_latex(bold_rows=True, column_format='rccc', na_rep='', index_names=False)
     tex = latex.render_templated_tex(template_tex, BatchID=batch_id, Date=date.strftime('%d.%m.%Y'), Filename=filename,
-                                     Comments=comments, Samples=samples)
+                                     Comments=comments, Samples=samples, LC_Settings=lc_settings)
     pdf = latex.pdflatex(tex=tex)
     return pdf
--- card_app/views.py
+++ card_app/views.py
@@ -4,13 +4,12 @@ from datetime import datetime
 from uuid import uuid4
 from flask import render_template, request, make_response
 import pandas as pd
+from XCaliburMethodReader import load_lc_data, get_lc_gradient, get_lc_settings
 from .card import df_to_card_pdf
 from . import app
 
 
 template_file = path.join(app.root_path, 'templates', 'card_template.tex')
-with open(template_file) as f:
-    template_tex = f.read()
 
 
 @app.route('/')
@@ -24,7 +23,16 @@ def upload():
     df = pd.read_csv(BytesIO(csv_data), skiprows=[0])
     batch_id = str(uuid4())[:5].upper()
     date = datetime.now()
-    pdf = df_to_card_pdf(template_tex=template_tex, df=df, filename=filename, batch_id=batch_id, date=date)
+
+    method_filename, method_data = request.files['method'].filename, request.files['method'].read()
+    lc_data = load_lc_data(BytesIO(method_data))
+    lc_settings = get_lc_settings(lc_data)
+
+    with open(template_file) as f:
+        template_tex = f.read()
+
+    pdf = df_to_card_pdf(template_tex=template_tex, df=df, filename=filename, batch_id=batch_id, date=date,
+                         lc_settings=lc_settings)
 
     response = make_response(pdf)
     response.headers['Content-Disposition'] = "inline; filename='booking.pdf"
