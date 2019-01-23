fe254e9f858d2bfa369b6d7d8f0bd3145de16670
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2019-01-23 13:09:24 +0100

    moved sequence structure creation to constructor and view formatting steps to the template.

--- card_app/card.py
+++ card_app/card.py
@@ -12,26 +12,20 @@ def df_to_xcalibur_csv(df, bracket=4):
 def df_to_card_pdf(template_tex, df, filename, batch_id, date, lc_settings, gradient,
                    post_fields=['Tip Box', 'Concentration Measurement Method', 'Measured Concentration', 'Predicted Sample Amount (ng)', 'LC Used', 'MS Used']):
 
-    comments = dict([el.strip() for el in item.split(':')] for item in df['Comment'][0].split(','))
-    samples = [row for _, row in df.iterrows()]
-    lc_settings.index = lc_settings.index.str.replace('reEquilibration', '')
-    lc_settings = lc_settings.to_latex(bold_rows=True, column_format='rccc', na_rep='', index_names=False)
-
     seq = Sequence(
         filename=filename,
         date=date,
         lc_settings=lc_settings,
         gradient=gradient,
-        comments=comments,
-        samples=samples,
+        comments=dict([el.strip() for el in item.split(':')] for item in df['Comment'][0].split(',')),
+        samples=[row for _, row in df.iterrows()],
         table=df,
     )
 
-    run_time = seq.run_time
-
-    tex = latex.render_templated_tex(template_tex, BatchID=batch_id, Date=date.strftime('%d.%m.%Y'), Filename=filename,
-                                     Comments=comments, Samples=samples, LC_Settings=lc_settings, Gradient=gradient,
-                                     PostFields=post_fields, RunTime=run_time)
+    tex = latex.render_templated_tex(template_tex, BatchID=seq.batch_id, Date=seq.date,
+            Filename=seq.filename, Comments=seq.comments, Samples=seq.samples, LC_Settings=seq.lc_settings,
+            Gradient=seq.gradient, PostFields=post_fields, RunTime=seq.run_time
+                                     )
 
 
     fig = utils.plot_gradient(gradient)
